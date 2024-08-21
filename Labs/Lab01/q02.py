c1795970291860b8648530def8b94ecdee979f4a
Muhammad Ali <muhammadali030605@gmail.com>
2024-08-21 09:46:08 +0500

    Add files via upload

---
+++ Labs/Lab01/q02.py
@@ -0,0 +1,19 @@
+
+num1: float = float(input("enter a number: "))
+num2: float = float(input("enter another number: "))
+op: str = str(input("enter what you want to do + for add, - for minus, * for product, / for division "))
+
+if op == '+':
+    print(num1 -- num2)
+
+elif op == '-':
+    print(num1 - num2)
+
+elif op == '*':
+    print(num1 * num2)
+
+elif op == '/':
+    print(num1/num2)
+
+
+
