bf3d3e66baf830a8ae5e4a271b19ffb0cfd307d7
Muhammadali0981 <muhammadali030605@gmail.com>
2024-08-24 23:12:14 +0500

    Done

--- Labs/Lab01/q11.py
+++ Labs/Lab01/q11.py
@@ -1,3 +1,28 @@
 # Name: Muhammad Ali
 # Date: 23/8/2024
-# Description: program to get the largest number from a list input from user.
+# Description: marks with user input
+
+data: dict[str, int] = {}
+
+total: int = 0
+avg: float = 0.0
+percentage: float = 0.0
+greatest: int = -99999
+max_marks_subject: str = ""
+
+for n in range(3):
+    key: str = str(input("enter subject name: "))
+    value: int = int(input("enter subject marks: "))
+
+    data[key] = value
+
+for key in data:
+    total += data[key]
+    if data[key] > greatest:
+        greatest = data[key]
+        max_marks_subject = key
+
+avg = total/len(data)
+percentage = total/300 * 100
+
+print(f'Average = {avg} and Percentage = {percentage} greatest marks in {max_marks_subject}')
---
+++ Labs/Lab01/q12.py
@@ -0,0 +1,14 @@
+# Name: Muhammad Ali
+# Date: 23/8/2024
+# Description: program to print a dictionary where the keys are numbers between 1 and 15
+#              and the values are the square of the keys.
+
+
+squares: dict[int, int] = {}
+
+for n in range(1,16):
+    key: int = n
+    value: int = n*n
+    squares[key] = value
+
+print(squares)
