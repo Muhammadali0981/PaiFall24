ba0f7687bc1b820b726b4041f9aa3a3a546e3877
Muhammadali0981 <muhammadali030605@gmail.com>
2024-12-03 18:48:26 +0500

    done

--- Labs/Lab11/q01.py
+++ Labs/Lab11/q01.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
@@ -6,7 +9,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 
-df = pd.read_csv('heart.csv')
+df = pd.read_csv(r'C:\me\PaiFall24\Labs\Lab11\heart.csv')
 
 
 X = df.drop('target',axis = 1)
@@ -16,14 +19,26 @@ Y = df['target']
 t_size = 0.20
 seed = 7
 
-X_train, Y_train, X_test, Y_test = train_test_split(X, Y, test_size=t_size, random_state=seed)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=t_size, random_state=seed)
 sc = StandardScaler()
 
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+X_train = sc.fit_transform(X_train) 
+X_test = sc.transform(X_test)  
+
+result = []
 
 for i in range (1, 251):
     knn = KNeighborsClassifier(n_neighbors=i)
     knn.fit(X_train, Y_train)
-    predictions = knn.predict(X_test)
-    print(accuracy_score(Y_test, predictions))
+    prediction = knn.predict(X_test)
+    accuracy = accuracy_score(Y_test, prediction)
+    result.append([i, accuracy])
+    print(f"k = {i}, Accuracy: {accuracy_score(Y_test, prediction):.4f}")
+
+result = pd.DataFrame(result, None ,['K', 'Accuracy']);
+max = result.loc[result['Accuracy'].idxmax()]
+min = result.loc[result['Accuracy'].idxmin()]
+print(f"Best K: {max['K']}, Max Accuracy: {max['Accuracy']:}")
+print(f"Worst K: {min['K']}, Max Accuracy: {min['Accuracy']:}");
+sns.regplot(x=result['K'],y = result["Accuracy"], line_kws={'color':'red'}, scatter_kws={'color':'darkblue', 'alpha':0.2, 'edgecolor':'black'})
+plt.show()
---
+++ Labs/Lab12/q01.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+
+df = pd.read_csv('Iris.csv')
+
+scalar = StandardScaler()
+df_scaled = scalar.fit_transform(df.copy(deep=True).iloc[:, :-1])
+
+pd.DataFrame(df_scaled).describe()
+
+SSE = []
+
+for i in range(1, 20):
+    kmeans = KMeans(n_clusters=i, init='k-means++')
+    kmeans.fit(df_scaled)
+    SSE.append(kmeans.inertia_)
+
+frame = pd.DataFrame({'Clusters': range(1, 20), 'SSE': SSE})
+plt.plot(frame['Clusters'], frame['SSE'])
+plt.show()
+
+kmeans = KMeans(n_clusters=3, init='k-means++')
+kmeans.fit(df_scaled)
+
+predict = kmeans.predict(df_scaled)
+frame = pd.DataFrame(df_scaled)
+
+frame['cluster'] = predict
+print(frame['cluster'].value_counts())
